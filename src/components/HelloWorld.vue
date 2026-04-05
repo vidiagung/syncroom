3411bec4c52c20abf726309f75e76e310227db43
VidiAgungAdhyaksa <kyoshirocommander@gmail.com>
2026-04-05 20:05:36 +0700

    chore(tsconfig): temporarily suppress baseUrl deprecation (TS 6)

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import viteLogo from '../assets/vite.svg'
-import heroImg from '../assets/hero.png'
-import vueLogo from '../assets/vue.svg'
+import viteLogo from '../images/vite.svg'
+import heroImg from '../images/hero.png'
+import vueLogo from '../images/vue.svg'
 
 const count = ref(0)
 </script>
